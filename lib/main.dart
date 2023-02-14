8833d79bc1aa49f105746e36518843a5bdb24f23
Gabriel Guedes <gabrielgue10@gmail.com>
2023-02-14 09:28:23 -0300

    test just_audio

---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:just_audio/just_audio.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final PageManager _pageManager = PageManager();
+
+  @override
+  void dispose() {
+    _pageManager.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ValueListenableBuilder<ProgressBarState>(
+              valueListenable: _pageManager.progressNotifier, 
+              builder: (_, value, __) {
+                return ProgressBar(
+                  onSeek: _pageManager.seek,
+                  progress: value.current,
+                  buffered: value.buffered,
+                  total: value.total,
+                );
+              }
+            ),
+            ValueListenableBuilder<ButtonState>(
+              valueListenable: _pageManager.buttonNotifier,
+              builder: (_, value, __) {
+                switch (value) {
+                  case ButtonState.finished:
+                    return IconButton(
+                      icon: const Icon(Icons.restart_alt),
+                      iconSize: 32.0,
+                      onPressed: _pageManager.play,
+                    );
+                  case ButtonState.loading:
+                    return Container(
+                      margin: const EdgeInsets.all(8.0),
+                      width: 32.0,
+                      height: 32.0,
+                      child: const CircularProgressIndicator(),
+                    );
+                  case ButtonState.paused:
+                    return IconButton(
+                      icon: const Icon(Icons.play_arrow),
+                      iconSize: 32.0,
+                      onPressed: _pageManager.play,
+                    );
+                  case ButtonState.playing:
+                    return IconButton(
+                      icon: const Icon(Icons.pause),
+                      iconSize: 32.0,
+                      onPressed: _pageManager.pause,
+                    );
+                }
+              },
+            ),
+          ]
+        )
+      ),
+    );
+  }
+}
+
+
+class PageManager {
+  static const url = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3';
+  late AudioPlayer _audioPlayer;
+
+  PageManager() {
+    _init();
+  }
+
+  final buttonNotifier = ValueNotifier<ButtonState>(ButtonState.paused);
+
+  final progressNotifier = ValueNotifier<ProgressBarState>(
+    ProgressBarState(
+      current: Duration.zero,
+      buffered: Duration.zero,
+      total: Duration.zero,
+    ),
+  );
+
+  void dispose() {
+    _audioPlayer.dispose();
+  }
+
+  void _init() async {
+    _audioPlayer = AudioPlayer();
+    await _audioPlayer.setUrl(url);
+
+    _audioPlayer.playerStateStream.listen((playerState) {
+      final isPlaying = playerState.playing;
+      final processingState = playerState.processingState;
+      if (processingState == ProcessingState.loading ||
+          processingState == ProcessingState.buffering) {
+        buttonNotifier.value = ButtonState.loading;
+      } else if (!isPlaying) {
+        if (_audioPlayer.duration != null && _audioPlayer.position == _audioPlayer.duration) {
+          buttonNotifier.value = ButtonState.finished;
+        } else {
+          buttonNotifier.value = ButtonState.paused;
+        }
+      } else if (processingState != ProcessingState.completed) {
+        buttonNotifier.value = ButtonState.playing;
+      } else { // completed
+        buttonNotifier.value = ButtonState.finished;
+        _audioPlayer.pause();
+      }
+    });
+
+    _audioPlayer.positionStream.listen((position) {
+      final oldState = progressNotifier.value;
+      progressNotifier.value = ProgressBarState(
+        current: position,
+        buffered: oldState.buffered,
+        total: oldState.total,
+      );
+    });
+
+    _audioPlayer.bufferedPositionStream.listen((bufferedPosition) {
+      final oldState = progressNotifier.value;
+      progressNotifier.value = ProgressBarState(
+        current: oldState.current,
+        buffered: bufferedPosition,
+        total: oldState.total,
+      );
+    });
+
+    _audioPlayer.durationStream.listen((totalDuration) {
+      final oldState = progressNotifier.value;
+      progressNotifier.value = ProgressBarState(
+        current: oldState.current,
+        buffered: oldState.buffered,
+        total: totalDuration ?? Duration.zero,
+      );
+    });
+  }
+
+  void play() {
+    if (_audioPlayer.position == _audioPlayer.duration) {
+      _audioPlayer.seek(Duration.zero);
+    }
+
+    _audioPlayer.play();
+  }
+
+  void pause() {
+    _audioPlayer.pause();
+  }
+
+  void seek(Duration position) {
+    _audioPlayer.seek(position);
+  }
+  
+}
+
+class ProgressBarState {
+  final Duration current;
+  final Duration buffered;
+  final Duration total;
+
+  ProgressBarState({
+    required this.current,
+    required this.buffered,
+    required this.total,
+  });
+}
+
+enum ButtonState {
+  paused, playing, loading, finished
+}
